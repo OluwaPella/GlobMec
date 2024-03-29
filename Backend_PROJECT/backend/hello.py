e2741d58c7bf405e85d6b8bf64b90acc692bb120
olaosebikan pelumi <oolaosebikan350@gmail.com>
2024-03-29 18:56:01 +0100

    add: updated file

---
+++ Backend_PROJECT/backend/hello.py
@@ -0,0 +1,6 @@
+x=5
+y=12
+if x or y in range(10):
+    print('u re good')
+else:
+    print("not in there")
--- queen/js/script.js
+++ queen/js/script.js
@@ -1,56 +1,56 @@
-fetch('http://127.0.0.1:5001/api/auth/Users')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-  
-const forms = document.querySelector(".forms"),
-    pwShowHide = document.querySelectorAll(".eye-icon"),
-    links = document.querySelectorAll(".link");
+// fetch('http://127.0.0.1:5001/api/auth/Users')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+// const forms = document.querySelector(".forms"),
+//     pwShowHide = document.querySelectorAll(".eye-icon"),
+//     links = document.querySelectorAll(".link");
 
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.querySelectorAll(".password");
-        pwFields.forEach(password => {
-            if(password.type === "password"){
-                password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
-        })
-    })
-})
+// pwShowHide.forEach(eyeIcon => {
+//     eyeIcon.addEventListener("click", () => {
+//         let pwFields = eyeIcon.parentElement.querySelectorAll(".password");
+//         pwFields.forEach(password => {
+//             if(password.type === "password"){
+//                 password.type = "text";
+//                 eyeIcon.classList.replace("bx-hide", "bx-show");
+//                 return;
+//             }
+//             password.type = "password";
+//             eyeIcon.classList.replace("bx-show", "bx-hide");
+//         })
+//     })
+// })
 
-links.forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault(); //prevent form submit
-        forms.classList.toggle("show-signup"); 
-    })
-})
+// links.forEach(link => {
+//     link.addEventListener("click", e => {
+//         e.preventDefault(); //prevent form submit
+//         forms.classList.toggle("show-signup"); 
+//     })
+// })
 
 
-var menu = document.getElementById("bar");
-var item = document.getElementById("item");
+// var menu = document.getElementById("bar");
+// var item = document.getElementById("item");
 
-item.style.right = '-300px';
+// item.style.right = '-300px';
 
-menu.onclick = function () {
-    if (item.style.right == '-300px') {
-        item.style.right = '0';
-    } 
-    else {
-        item.style.right == '-300px';
-    }
-} 
+// menu.onclick = function () {
+//     if (item.style.right == '-300px') {
+//         item.style.right = '0';
+//     } 
+//     else {
+//         item.style.right == '-300px';
+//     }
+// } 
+console.log("doc:",document.getElementById('registrationForm'))
 
 document.getElementById('registrationForm').addEventListener('submit', function (event) {
     event.preventDefault();
-ls
+
     const formData = new FormData(event.target);
 
     const jsonData = {};
